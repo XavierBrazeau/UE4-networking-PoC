ad0933f9e12fe0a0a220957944e2b91ae6fa5908
Emmanuelle Roberge <emmanuelle.roberge@polymtl.ca>
2021-02-15 19:49:08 -0500

    ER : Avancements dans les tests

---
+++ Project/Source/MyProject4/RealGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "RealGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT4_API URealGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
